1f7daf42551fe1b84b0a7791d489a0f3ac9b3ab0
HeroreH <aykuthero@gmail.com>
2026-05-22 02:12:27 +0300

    Create forcycleinlists.py

---
+++ forcycleinlists.py
@@ -0,0 +1,6 @@
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for i in range(len(supplies)):
+    print('index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+#len(supplies) is equal to the integer 4. So for cycle turns from 0 to 3. And in every cycle \
+# it prints index name, list' s name and the argument respectively. 
