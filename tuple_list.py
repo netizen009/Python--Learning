0cba877398216b7cdf758162f9d4e7629c756ea0
HeroreH <aykuthero@gmail.com>
2026-05-22 01:58:12 +0300

    Create tuple_list.py

---
+++ tuple_list.py
@@ -0,0 +1,6 @@
+a = ('hello','hi')
+print(type(a)) # <class 'tuple'>
+b = ['hello','hi']
+print(type(b)) # <class 'list'>
+#List and Tuple Type Conversion
+print(type(tuple(b))) # <class 'tuple'> which means list class has turned to tuple class
