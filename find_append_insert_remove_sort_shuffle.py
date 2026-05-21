ae9bb02b3e0fed9e51d439bd9a3ee11594f0ed13
HeroreH <aykuthero@gmail.com>
2026-05-22 01:50:33 +0300

    Create find_append_insert_remove_sort_shuffle.py

---
+++ find_append_insert_remove_sort_shuffle.py
@@ -0,0 +1,8 @@
+spam = ['hello', 'hi', 'howdy', 'heyas', 'hi']
+print(spam.index('hi') #1, only first item is shown
+spam.append(3) #3,  The append() method adds the argument to the end of the list
+spam.insert(2, 3) #The insert() method inserts the 3 value at second index in the list
+spam.remove('hi') #first index argument is removed
+spam.sort() #The method returns the numbers in numerical order and the strings in alphabetical order.
+spam.sort(reverse=True) #The values is sorted in reverse order
+spam.reverse() #quickly reverse the order of the items in a list without sorting them
