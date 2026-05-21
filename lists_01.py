0d285c69d2fcfa0d8c5f5d01660d6027eb428a4b
HeroreH <aykuthero@gmail.com>
2026-05-22 01:31:33 +0300

    Create lists_01.py

---
+++ lists_01.py
@@ -0,0 +1,10 @@
+#list indexes 
+spam = [[1, 2], 'cat', 'bat', 17, 'elephant']
+print(spam[0]) # [1, 2]
+print(spam[0][1]) # 2
+print(spam[2]) # bat
+#concatenation and replication of lists 
+spam= [1, 2, 3]
+spam_1= ['a', 'b', 'c']
+print(spam + spam_1)
+print(spam*3)
